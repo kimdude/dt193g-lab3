958ac6d406b534d255859b3a171aa482944c060b
Kim Dudenhöfer <kidu2400@student.miun.se>
2025-12-12 15:18:48 +0100

    Lagt till info på AboutView.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,25 @@
 <template>
-    <h2>Om labben</h2>
+    <section>
+        <div>
+            <h2>Om labben</h2>
+            <p>
+                Det har gått bra att arbeta med Vue.js. Ramverket påminner mycket om Angular som jag arbetat i några gånger tidigare. Det som skiljde sig fanns video instruktioner
+                för i läsanvisningar, vilket underlättade mycket. Allt rullade på utan problem.
+            </p>
+            <p>
+                En sak som kändes lite svårare var emits, även om Angular också har en version av dessa. Sedan kändes placering av bilder förvirrande, att både public och assets
+                finns. Jag har än så länge inte riktigt förstått poängen med det och blir förvirrad över filvägen.
+            </p>
+            <p> Trots det har det varit väldigt kul att arbeta med Vue.js och jag kommer nog testa det fler gånger framöver!</p>
+        </div>
+    </section>
 </template>
 
-<script setup>
-
-</script>
-
 <style scoped>
-    
+    div {
+        display: block;
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+    }
 </style>
